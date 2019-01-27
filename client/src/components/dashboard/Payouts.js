306016721c0cebfa3099c68e025b34f9c305cb8b
Simba <simba.fan@outlook.com>
2019-01-28 09:50:39 +1000

    Payouts.js modified

--- client/src/components/dashboard/Payouts.js
+++ client/src/components/dashboard/Payouts.js
@@ -1,17 +1,6 @@
 import React from "react";
 
 class Payouts extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			page: 0,
-			rowsPerPage: 1,
-			selectedId: "",
-			tableData: [],
-			statisticsTable: []
-		};
-	}
-
 	render() {
 		return <div>Payouts</div>;
 	}
